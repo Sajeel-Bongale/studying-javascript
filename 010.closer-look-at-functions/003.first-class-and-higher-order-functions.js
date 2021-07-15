69d9901cf23b088e0f58d476ae805b8f383f8288
Sajeel Bongale <36545436+Sajeel-Bongale@users.noreply.github.com>
2021-07-15 21:00:04 +0530

    Updates first class and higher order functions

--- 010.closer-look-at-functions/003.first-class-and-higher-order-functions.js
+++ 010.closer-look-at-functions/003.first-class-and-higher-order-functions.js
@@ -2,4 +2,6 @@
     Link: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648649#overview
     
     First class functions is a concept in programming while higher order functions have actual implementations.
+    
+    Higher order functions are possible because the language supports first class functions.
  */
