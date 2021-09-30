537e7648be6897a260d6564cbb98b96b65a17e5d
Sajeel-Bongale <sajeelbongale@gmail.com>
2021-09-30 14:21:15 +0530

    Adds new array methods

--- general-stuff-that-you-forget/004.array-methods.js
+++ general-stuff-that-you-forget/004.array-methods.js
@@ -19,4 +19,17 @@
     join() -                                    Returns a new string by concatenating all of the elements in an array
     Params - separator                          separated by commas(default) or a specified separator string
 
+    map() -                                     Creates a new array populated with the results of calling a provided
+    Params - mapper callback                    function on every element in the calling array
+
+    filter() -                                  Creates a new array with all elements that pass the test
+    Params - filter callback                    implemented by the provided function
+
+
+    reduce() -                                  Executes a user-supplied “reducer” callback function on each element of
+    Params - reducer callback, accumulator      the array, passing in the return value from the calculation on the
+                                                preceding element. The final result of running the reducer across all
+                                                elements of the array is a single value.
+
+
  */
