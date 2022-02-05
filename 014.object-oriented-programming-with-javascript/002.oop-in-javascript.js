868749a1cfff8be0419f516f59a85ecf62dc180e
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-02-05 16:54:37 +0530

    OOP in Javascript

---
+++ 014.object-oriented-programming-with-javascript/002.oop-in-javascript.js
@@ -0,0 +1,25 @@
+/*
+    Link: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649037#overview
+
+    A class is like a blueprint which can be used to create instances.
+    This process of creating objects is called instantiation.
+
+    How does OOP work in Javascript?
+    In javascript there is a concept called prototype and all objects in JS are linked to a
+    certain prototype object.
+
+    Each object has a prototype.
+
+    The prototype object contains properties and methods that each object of that class can use and access.
+    This is called prototypal inheritance.
+
+    The inheritance is also called delegation in JS.
+
+    The 3 ways of implementing prototypal inheritance are:
+    1. Constructor functions
+    2. ES6 Classes
+    3. Object.create()
+
+    Objects inherit properties and methods from the prototype.
+
+ */
