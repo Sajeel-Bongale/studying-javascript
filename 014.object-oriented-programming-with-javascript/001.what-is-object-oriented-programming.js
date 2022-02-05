bb51fcc9d890ba66f68ced1726a26b79e2ceb267
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-02-05 16:38:09 +0530

    What is object oriented programming

---
+++ 014.object-oriented-programming-with-javascript/001.what-is-object-oriented-programming.js
@@ -0,0 +1,41 @@
+/*
+    Link: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649033#overview
+
+    What is object oriented programming (OOP)?
+    OOP is a programming paradigm based on the concept of objects.
+
+    Objects are used to model real world features like user
+
+    Objects contain data(properties) and code(methods)
+
+    In OOP objects are self contained pieces of code that are building blocks for applications and
+    they interact with each other.
+
+    Interactions such as these happen through a public interface(API): methods that the code
+    outside of the object can access and use to communicate with the object.
+
+    In traditional OOP we use classes to create objects. Classes are like blueprints which are used
+    to generate objects of that type. It is like a set of rules which are abstract.
+
+    All objects created using classes are called instances of that class.
+
+    The class itself is not an object.
+
+    The 4 fundamental principles of OOP are Abstraction, Encapsulation, Inheritance and Polymorphism.
+
+    Abstraction means to ignore or to hide details that don't matter. This allows us to get an overview perspective
+    of whatever it is that we're implementing instead of messing with details that don't really matter to our
+    implementation.
+
+    Encapsulation means keeping some properties and method private inside the class so they are not accessible from
+    outside the class. Some methods can be exposed as a public interface(API).
+
+    Inheritance makes all properties and methods of a class to all child classes. It thus, creates a hierarchical
+    relationship between classes. This allows us to reuse common logic.
+
+    Polymorphism means a child class can overwrite a method inherited from a parent class.
+
+    Note: Javascript does not support real classes. It does have the syntax that looks like it is creating
+    real classes, however that is not the case.
+
+ */
