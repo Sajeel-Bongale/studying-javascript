c86454635377b16c6a4e103f72117642e34348f6
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-02-09 16:11:25 +0530

    Updates converting and checking numbers

---
+++ general-stuff-that-you-forget/005.number-methods.js
@@ -0,0 +1,30 @@
+/*
+
+    Useful Number methods
+
+    // constructor
+    Number() -                                  Number is a primitive wrapper object  used to represent and manipulate
+    Params - value                              numbers like 37 or -9.25. Values of other types can be converted to
+                                                numbers using the Number() function. It is a double-precision 64-bit
+                                                binary format IEEE 754  value. This means it can represent fractional
+                                                values, but there are some limits to what it can store.
+
+    parseInt() -                                Parses a string argument and returns an integer of the specified
+    Params - valueInString, radix               radix or base. The value must start with a number else the result is
+                                                NaN. The default radix is 10. Passing a string with floating point
+                                                number in it will result in an integer without the fractional part.
+
+    parseFloat() -                              Parses an argument and returns a floating point number. If a number
+    Params - valueInString                      cannot be parsed from the argument, it returns NaN.
+
+    isNaN() -                                   Determines whether the passed value is NaN and its type is Number.
+    Params - valueToTest                        It is a more robust version of the original, global isNaN().
+
+    isFinite() -                                Determines whether the passed value is a finite number — that is,
+    Params - valueToTest                        it checks that the type of a given value is Number, and the number
+                                                is neither positive Infinity, negative Infinity, nor NaN. This is
+                                                a better check than isNaN to check if a value is an Number or not.
+
+    isInteger() -                               Determines whether the passed value is an integer.
+    Params - valueToTest
+*/
