88ce34aaa2824223fb9f850b323750e5b4f8e558
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-06-15 17:15:50 +0530

    Updates object methods

--- general-stuff-that-you-forget/002.object-methods.js
+++ general-stuff-that-you-forget/002.object-methods.js
@@ -1,6 +1,6 @@
 /*
 
-    Useful Object methods:
+    Useful Object methods
 
     Object.assign(target, ...sources) - Copies the values of all enumerable own properties from one or
                                         more source objects to a target object. Creates a shallow copy
@@ -14,13 +14,17 @@
     Object.keys(obj) -                  Returns an array containing the names of all of the given object's
                                         own enumerable string properties.
 
+    Object.values(obj) -                Returns an array containing the values that correspond to all of a
+                                        given object's own enumerable string properties.
+
+    Object.freeze(obj) -                Freezes an object. A frozen object can no longer be changed. Freezing an
+                                        object prevents new properties from being added to it and existing properties
+                                        from being removed. Only shallow freezes an object.
+
     Object.getOwnPropertyNames(obj) -   Returns an array containing the names of all of the given object's
                                         own enumerable and non-enumerable properties.
 
     Object.defineProperty(obj, prop, -  Adds the named property described by a given descriptor to an object.
     descriptor)                         Can be used to add enumerable and non enumerable property to the object.
-
-    Object.values(obj) -                Returns an array containing the values that correspond to all of a
-                                        given object's own enumerable string properties.
 
  */
