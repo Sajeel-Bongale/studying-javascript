b23d9154edf43cb7a640df06d2a3ff11cd334909
Sajeel-Bongale <sajeelbongale@gmail.com>
2022-09-24 13:25:13 +0530

    ES Updates

---
+++ general-stuff-that-you-forget/007.es-updates.js
@@ -0,0 +1,68 @@
+/*
+    Additions to JS as per new updates
+
+    ES6 (ES2015)
+    1. let, const
+    2. Arrow functions
+    3. Template literals
+    4. Object and array destructuring
+    5. Promises
+    6. Default function parameters
+    7. Class syntax
+    8. Rest parameter and spread operator
+    9. Modules import and export
+    10. for...of loop syntax
+
+
+    ES7 (ES2016)
+    1.  Exponentiation operator - 10 ** 3
+    2.  Array.prototype.includes method - [1,2,3].includes(1)
+
+
+    ES8 (ES2017)
+    1. async/await
+    2. Object.entries and Object.values static methods
+    3. String padding myString.padStart(2) or myString.padEnd(2);
+    4. Trailing comma function test(a,b,c, )
+
+
+    ES9 (ES2018)
+    1. Asynchronous iteration - for await (const line of readlines(filePath)) {}
+    2. Rest operators can be used as last items in function params - function restParamsEx(p1, p2, ...p3)
+    3. Promise.prototype.finally method
+    4. Spread operators on objects
+
+
+    ES10 (ES2019)
+    1. Array.prototype.flat and Array.prototype.flatMap methods
+    2. Object.fromEntries static method - reverse operation from Object.entries (see here)
+    3. String.prototype.trimStart and String.prototype.trimEnd methods- Remove extra spaces in a string
+    4. Optional Catch binding: remove the need to add a param to the catch (Now you can do } catch {instead of } catch(e) {
+    5. Function.prototype.toString method - returns string representation of function source code
+
+
+    ES11 (ES2020)
+    1. Private class variables class Person { #dateOfBirth = '1980-05-27' }
+    2. Promise.prototype.allSettled method - runs all promises regardless of rejection of any promises
+    3. String.prototype.matchAll method
+    4. Optional Chaining operator - myobject?.possibleExistingProperty
+    5. Dynamic Imports - Lazy loading imports for better performance
+    1. BigInt datatype - 9007199254740992n
+
+
+    ES12 (ES2021)
+    1. Numeric separators - (_) 100_000_000_000
+    2. Promise.prototype.any - returns first fulfilled promise
+    3. Private class methods - class Person { #age() {return 10} }
+    4. Logical assignment operators - ??=, &&=, ||=
+    5. String.prototype.replaceAll method - 'hotdog dog'.replaceAll('dog', 'cat');
+
+
+    ES13 (ES2022)
+    1. Top level await
+    2. Class field declarations - class User { name = "Tom" }
+    3. Class Static Block - class Words { static { const words = []; } }
+    4. Array.prototype.at method - [1,2,3].at(1)
+    5. Array.prototype.findLast and findLastIndex methods - array1.findLast((element) => element > 45);
+
+ */
